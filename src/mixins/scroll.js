7a4e5035fa38dc9669e5400775656d58177d77bc
BabzT <babatumdehezekiah7@gmail.com>
2022-08-11 16:32:25 +0100

    fix

--- src/mixins/scroll.js
+++ src/mixins/scroll.js
@@ -3,23 +3,23 @@ export default{
         // for topsellers
         scrollRight1(){
             let content = document.querySelector('.toppicks')
-            content.scrollLeft += 100
+            content.scrollLeft += 200
             console.log('scroll')
         },
         scrollLeft1(){
             let content = document.querySelector('.toppicks')
-            content.scrollLeft -=100
+            content.scrollLeft -=200
             console.log('scroll')
         },
         // for trending products
         scrollRight2(){
             let content = document.querySelector('.electronics')
-            content.scrollLeft += 100
+            content.scrollLeft += 200
             console.log('scroll')
         },
         scrollLeft2(){
             let content = document.querySelector('.electronics')
-            content.scrollLeft -=100
+            content.scrollLeft -=200
             console.log('scroll2')
         },
     }
